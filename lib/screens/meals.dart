c01fd54d916db06cd6a8f97e305f39b12d3d547a
Kevin Machado <kevinm4ch@gmail.com>
2024-11-11 13:30:00 -0300

    first commit

---
+++ lib/screens/meals.dart
@@ -0,0 +1,57 @@
+import 'package:app_receitas/widgets/meal_item.dart';
+import 'package:flutter/material.dart';
+import '../models/meal.dart';
+
+class ComidaScreen extends StatelessWidget {
+  const ComidaScreen({
+    super.key,
+    required this.title,
+    required this.meals,
+  });
+
+  final String title;
+  final List<Meal> meals;
+
+  @override
+  Widget build(BuildContext context) {
+    Widget content = Center(
+      child: Column(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          Text(
+            'Nothing to show!',
+            style: Theme.of(context)
+                .textTheme
+                .headlineLarge!
+                .copyWith(color: Theme.of(context).colorScheme.onBackground),
+          ),
+          const SizedBox(
+            height: 16,
+          ),
+          Text(
+            'Try selecting a different category!',
+            style: Theme.of(context)
+                .textTheme
+                .bodyLarge!
+                .copyWith(color: Theme.of(context).colorScheme.onBackground),
+          ),
+        ],
+      ),
+    );
+
+    if (meals.isNotEmpty) {
+      content = ListView.builder(
+        itemCount: meals.length,
+        itemBuilder: (ctx, index) => MealItem(meal: meals[index]),
+      );
+    }
+    //Text(meals[index].title)
+
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(title),
+      ),
+      body: content,
+    );
+  }
+}
---
+++ lib/widgets/category_grid_item.dart
@@ -0,0 +1,53 @@
+import 'package:app_receitas/data/dummy_data.dart';
+import 'package:app_receitas/screens/meals.dart';
+import 'package:flutter/material.dart';
+import '../models/category.dart';
+
+class CategoryGridItem extends StatelessWidget {
+  const CategoryGridItem({super.key, required this.category});
+
+  final Category category;
+
+  @override
+  Widget build(BuildContext context) {
+    return InkWell(
+      onTap: () {
+        Navigator.push(
+          context,
+          MaterialPageRoute(
+              builder: (context) =>
+                  ComidaScreen(
+                    title: category.title, 
+                    meals: dummyMeals
+                      .where((m) => m.categories.contains(category.id))
+                      .toList(),
+            ),
+          ),
+        );
+      },
+      splashColor: Theme.of(context).primaryColor,
+      borderRadius: BorderRadius.circular(16),
+      child: Container(
+        padding: const EdgeInsets.all(16),
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(16),
+          gradient: LinearGradient(
+            colors: [
+              category.color.withOpacity(0.55),
+              category.color.withOpacity(0.9),
+            ],
+            begin: Alignment.topLeft,
+            end: Alignment.bottomRight,
+          ),
+        ),
+        child: Text(
+          category.title,
+          style: Theme.of(context)
+              .textTheme
+              .titleLarge!
+              .copyWith(color: Theme.of(context).colorScheme.onBackground),
+        ),
+      ),
+    );
+  }
+}
